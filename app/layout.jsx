5725182123617e29148ec9d28a0aa30fe30a41b7
Sushi <shaheer.anser@gmail.com>
2024-12-01 12:13:55 +0500

    prisma setup

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,7 @@
 import "@styles/globals.css";
 
+import Navbar from "@components/Navbar";
+
 export const metadata = {
   title: "",
   description: "",
@@ -8,7 +10,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Navbar />
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
